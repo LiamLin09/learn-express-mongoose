784bd88249aa4dfa2866b3b0e27a8f83a5202b2a
LiamLin <137009133+LiamLin09@users.noreply.github.com>
2024-03-13 20:00:29 -0400

    Update book.js

--- models/book.js
+++ models/book.js
@@ -1,9 +1,17 @@
 var mongoose = require('mongoose');
+const author = require('./author');
+const genre = require('./genre');
 
 var Schema = mongoose.Schema;
 
 var BookSchema = new Schema(
-  {}
+  {
+    title: {type: String, required: true},
+    author: {type: Schema.type.ObjectId, ref: 'Author', required: true},
+    summary: {type: String, required: true},
+    ISBN: {type: String, required: true},
+    genre: [{type: Schema.type.ObjectId, ref: 'Genre'}],
+  }
 );
 
 //Export model
